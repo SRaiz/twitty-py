424d8367cfa8e3316552f745d14c6147f7ae0348
SRaiz <sidharthraizada07@gmail.com>
2020-02-22 22:05:57 +0000

    Updated Twitty

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('authenticate', views.authenticate_twitter, name='authenticate')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'pageheader': 'Dashboard'})
+
+
+def authenticate_twitter(request):
+    return HttpResponse('Will authenticate now')
