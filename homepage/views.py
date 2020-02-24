397a8d1ba4d5823df554e29bd24f07e688a7a947
SRaiz <sidharthraizada07@gmail.com>
2020-02-24 18:54:18 +0000

    Updated Consumer Key

--- homepage/views.py
+++ homepage/views.py
@@ -13,10 +13,9 @@ def authenticate_twitter(request):
     #   Get all the environment variables
     consumer_key = os.environ.get('CONSUMER_KEY')
     consumer_secret = os.environ.get('CONSUMER_SECRET')
-    print(consumer_key, consumer_secret)
 
     #   Call the OAuth2 authentication using tweepy
-    auth = tweepy.AppAuthHandler('7PUjGmKSz6d6s8SbncTjtZ7Oy', 'TNKgr6zPHyxvc1JCH0N1QXnl89zvt5c6J7oxcPQXKfwYvnFxe4')
+    auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
     api = tweepy.API(auth, wait_on_rate_limit = True)
 
     #   The search term and date from which data is required. Retweets are filtered here
@@ -24,21 +23,24 @@ def authenticate_twitter(request):
     date_since = '2020-02-01'
 
     #   Collect Tweets
-    tweets = tweepy.Cursor( api.search, q = search_words, lang = 'en', since = date_since ).items(5)
+    tweets = tweepy.Cursor( api.search, q = search_words, lang = 'en', since = date_since, tweet_mode = 'extended' ).items(5)
 
     tweets_to_show = []
     for tweet in tweets:
+        # print('================')
+        # print(tweet.entities.unwound.url)
+        # print('================')
         temp_tweet = {
             'created_at': tweet.created_at,
             'id': tweet.id,
-            'text': tweet.text,
+            'text': tweet.full_text,
             'user': {
                 'name': tweet.user.name,
                 'screen_name': tweet.user.screen_name,
                 'location': tweet.user.location,
                 'description': tweet.user.description
-            },
-            'url': tweet.urls[0].url
+            }
+            #'url': tweet.entities.urls
         }
         tweets_to_show.append(temp_tweet)
 
