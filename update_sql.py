5e9d231334b73bdace1de5d1ade71ab74d50abbd
SRaiz <sidharthraizada07@gmail.com>
2020-02-25 09:09:51 +0000

    Update tweety

--- update_sql.py
+++ update_sql.py
@@ -1,14 +1,12 @@
-import csv
-import sqlite3
-
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
 
 
 def update_tweets( tweets ):
     
-    db_filename = 'db.sqlite3'
-    conn = sqlite3.connect( db_filename )
+    engine = create_engine('postgresql+psycopg2://postgres:tweety_336600@localhost:5432/tweety-py')
 
     #   Store tweets in dataframe and send it to the database
     tweets_df = pd.DataFrame(tweets)
-    tweets_df.to_sql('homepage_tweet', conn, if_exists='replace')
+    tweets_df.to_sql('homepage_tweet', engine, if_exists='replace')
